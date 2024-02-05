04a10df8a88498f645ca5a6e52075a78148804b0
Daniel Peshkov <109922163+DanielPeshkov@users.noreply.github.com>
2024-02-05 14:51:34 -0500

    first commit

---
+++ simple-sentiment.py
@@ -0,0 +1,19 @@
+# Installing transformers library is necessary in Kaggle notebooks
+# !pip install -q transformers
+
+# Imports
+import pandas as pd
+from transformers import pipeline
+
+# Load the files
+test = pd.read_csv('test.csv')
+sample = pd.read_csv('sample_submission.csv')
+
+# Create the sentiment analysis pipeline and make predictions
+sentiment_pipeline = pipeline("sentiment-analysis")
+predictions = sentiment_pipeline(list(test['text']))
+labels = [1*(i['label'] == 'NEGATIVE') for i in predictions]
+
+# Write predictions to submission file
+sample['target'] = labels
+sample[['id', 'target']].to_csv('submission.csv', index=False)
